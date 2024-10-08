cf205b208d7d245f246045d2f16e620b194ab14d
Anon23261 <anonymous23261@icloud.com>
2024-10-07 22:55:53 -0600

    Initial commit

---
+++ Thegreatestshowdown.py
@@ -0,0 +1,16 @@
+# Task 1: Identify The Greatest
+num1 = float(input("Enter the first number for Task 1: "))
+num2 = float(input("Enter the second number for Task 1: "))
+num3 = float(input("Enter the third number for Task 1: "))
+
+largest = max(num1, num2, num3)
+print("The largest number is:", largest)
+
+# Task 2: Identify The Smallest
+num1 = float(input("Enter the first number for Task 2: "))
+num2 = float(input("Enter the second number for Task2: "))
+num3 = float(input("Enter the third number for Task 2: "))
+num4 = float(input("Enter the fourth number for Task 2: "))
+
+smallest = min(num1, num2, num3, num4)
+print("The smallest number is:", smallest)
